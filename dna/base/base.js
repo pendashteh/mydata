20b0fc869269b7f5b8bc330ff4084b681167ad88
Alexar <github@alexar.me>
2018-03-22 07:57:35 +1100

    MyData version 1.

---
+++ dna/base/base.js
@@ -0,0 +1,184 @@
+'use strict';
+
+function usernameCreate(username) {
+    var usernameHash = commit("username", username);
+    commit("reference", {Links: [{
+        Base: App.Key.Hash,
+        Link: usernameHash,
+        Tag: 'username'
+    }]});
+    return usernameHash;
+}
+function usernameGetMine() {
+    var links = getLinks(App.Key.Hash, 'username', {Load: true});
+    for (var i in links) {
+        return links[i]["Entry"];
+    }
+    return null;
+}
+function publicEntryCreate(publicEntry) {
+    var publicEntryHash = commit("publicEntry", publicEntry);
+    var base = makeHash('username', usernameGetMine());
+    commit("reference", {Links:[{
+        Base: base,
+        Link: publicEntryHash,
+        Tag: 'public_entry'
+    }]});
+    return publicEntryHash;
+}
+
+function publicEntryListMine() {
+    var username = usernameGetMine();
+    return publicEntryListByUser(username);
+}
+
+function publicEntryListByUser(username) {
+    var usernameHash = makeHash('username', username);
+    var links = getLinks(usernameHash, 'public_entry', {Load: true});
+    var entries = [];
+    for (var i in links) {
+        var entry = links[i]["Entry"];
+        entries.push(entry);
+    }
+    return entries;
+}
+
+// -----------------------------------------------------------------
+//  The Genesis Function https://developer.holochain.org/genesis
+// -----------------------------------------------------------------
+
+/**
+* Called only when your source chain is generated
+* @return {boolean} success
+*/
+function genesis () {
+    return true;
+}
+
+// -----------------------------------------------------------------
+//  Validation functions for every change to the local chain or DHT
+// -----------------------------------------------------------------
+
+function _baseValidateDefault(entryName, entry, header, pkg, sources) {
+    // @FIXME consider!
+    switch (entryName) {
+        case "publicEntry":
+            return true;
+        case "username":
+            return true;
+        case "reference":
+            return true;
+        default:
+        // invalid entry name
+            return false;
+    }
+}
+function _baseValidateLinkDefault(entryName, entry, header, pkg, sources) {
+    switch (entryName) {
+        default:
+        // invalid entry name
+            return false;
+    }
+}
+
+/**
+* Called to validate any changes to the local chain or DHT
+* @param {string} entryName - the type of entry
+* @param {*} entry - the entry data to be set
+* @param {object} header - header for the entry containing properties EntryLink, Time, and Type
+* @param {*} pkg - the extra data provided by the validate[X]Pkg methods
+* @param {object} sources - an array of strings containing the keys of any authors of this entry
+* @return {boolean} is valid?
+*/
+function validateCommit(entryName, entry, header, pkg, sources) {
+    return _baseValidateDefault(entryName, entry, header, pkg, sources);
+}
+
+/**
+* Called to validate any changes to the local chain or DHT
+* @param {string} entryName - the type of entry
+* @param {*} entry - the entry data to be set
+* @param {object} header - header for the entry containing properties EntryLink, Time, and Type
+* @param {*} pkg - the extra data provided by the validate[X]Pkg methods
+* @param {object} sources - an array of strings containing the keys of any authors of this entry
+* @return {boolean} is valid?
+*/
+function validatePut (entryName, entry, header, pkg, sources) {
+    return _baseValidateDefault(entryName, entry, header, pkg, sources);
+}
+
+/**
+* Called to validate any changes to the local chain or DHT
+* @param {string} entryName - the type of entry
+* @param {*} entry - the entry data to be set
+* @param {object} header - header for the entry containing properties EntryLink, Time, and Type
+* @param {string} replaces - the hash for the entry being updated
+* @param {*} pkg - the extra data provided by the validate[X]Pkg methods
+* @param {object} sources - an array of strings containing the keys of any authors of this entry
+* @return {boolean} is valid?
+*/
+function validateMod (entryName, entry, header, replaces, pkg, sources) {
+    return _baseValidateDefault(entryName, entry, header, pkg, sources);
+}
+
+/**
+* Called to validate any changes to the local chain or DHT
+* @param {string} entryName - the type of entry
+* @param {string} hash - the hash of the entry to remove
+* @param {*} pkg - the extra data provided by the validate[X]Pkg methods
+* @param {object} sources - an array of strings containing the keys of any authors of this entry
+* @return {boolean} is valid?
+*/
+function validateDel (entryName, hash, pkg, sources) {
+    return _baseValidateDefault(entryName, entry, header, pkg, sources);
+}
+
+/**
+* Called to validate any changes to the local chain or DHT
+* @param {string} entryName - the type of entry
+* @param {string} baseHash - the hash of the base entry being linked
+* @param {?} links - ?
+* @param {*} pkg - the extra data provided by the validate[X]Pkg methods
+* @param {object} sources - an array of strings containing the keys of any authors of this entry
+* @return {boolean} is valid?
+*/
+function validateLink (entryName, baseHash, links, pkg, sources) {
+    return true;
+    return _baseValidateLinkDefault(entryName, entry, header, pkg, sources);
+}
+
+/**
+* Called to get the data needed to validate
+* @param {string} entryName - the name of entry to validate
+* @return {*} the data required for validation
+*/
+function validatePutPkg (entryName) {
+    return null;
+}
+
+/**
+* Called to get the data needed to validate
+* @param {string} entryName - the name of entry to validate
+* @return {*} the data required for validation
+*/
+function validateModPkg (entryName) {
+    return null;
+}
+
+/**
+* Called to get the data needed to validate
+* @param {string} entryName - the name of entry to validate
+* @return {*} the data required for validation
+*/
+function validateDelPkg (entryName) {
+    return null;
+}
+
+/**
+* Called to get the data needed to validate
+* @param {string} entryName - the name of entry to validate
+* @return {*} the data required for validation
+*/
+function validateLinkPkg (entryName) {
+    return null;
+}
---
+++ ui/script.js
@@ -0,0 +1,64 @@
+var display_message = function(text) {
+    document.getElementById("message").innerHTML = '<p>' + text + '</p>';
+}
+var hc_post = function(command, input, fn) {
+    var input = input || {};
+    xhr_post("fn/base/"+command, input, function(data) {
+        var message = [];
+        message.push("<b>Command:</b> " + command);
+        message.push("<b>Input:</b> " + JSON.stringify(input));
+        message.push("<b>Response:</b> " + data);
+        display_message(message.join("<br>"));
+        !fn || fn(data);
+        return data;
+    });
+}
+var xhr_post = function(url, input, callback) {
+    var xhr = new XMLHttpRequest()
+    xhr.open('POST', url, true)
+    xhr.setRequestHeader('Content-type', 'application/json')
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        callback(xhr.responseText)
+      }
+    }
+    var data = JSON.stringify(input);
+    xhr.send(data)
+}
+var button_onclick = function(id, callback) {
+    document.getElementById(id).addEventListener('click', callback);
+}
+var input_value = function(name) {
+    var inputs = document.getElementsByName(name);
+    for (i in inputs) {
+        return inputs[i].value;
+    }
+    return null;
+}
+button_onclick("button-adddata", function(e) {
+    var entry_fieldname = input_value('entry_fieldname');
+    var entry_value = input_value('entry_value');
+    var entry = {
+        "fieldname": entry_fieldname,
+        "value": entry_value
+    };
+    hc_post("publicEntryCreate", entry);
+    return false;
+});
+button_onclick("button-setusername", function(e) {
+    var username = input_value('username');
+    hc_post("usernameCreate", username);
+    return false;
+});
+button_onclick("button-listdata", function(e) {
+    hc_post("publicEntryListMine", {}, function(data) {
+        var result = JSON.parse(data);
+        var items = [];
+        for (i in result) {
+            var entry = result[i];
+            items.push(entry.fieldname + ": " + entry.value);
+        }
+        document.getElementById("x-listdata").innerHTML = items.join("<br>");
+    });
+    return false;
+});
